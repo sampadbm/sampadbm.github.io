e3ee78efab26ea97d4ed90b48e9f8228364cfe02
sampad <smpdmohanty@gmail.com>
2020-07-27 13:51:49 -0700

    added themes

---
+++ __resources/sampy/js/main.js
@@ -0,0 +1,126 @@
+ISA_DEBUG=0
+
+
+function extract_front_matter(data){
+    data = data.trim()
+    lines = data.split("\n")
+ 
+    frontmatter={};
+
+    //check if front matter exists
+    if(lines[0].trim().includes("---"))
+    {
+      nlines = lines.length;
+      lines = lines.splice(1,nlines)
+      trimmedlines = lines.map(l => l.trim())
+
+      end = trimmedlines.indexOf("---")
+
+      frontmatter_text = lines.splice(0,end).join("")
+      if(ISA_DEBUG)console.log("FRONT MATTER TEXT ->\n" + frontmatter_text);
+
+      if(frontmatter_text) {
+          __frontmatter = jsyaml.load(frontmatter_text)
+          if(__frontmatter) frontmatter = __frontmatter;
+      }
+
+      data =   lines.splice(end+1,nlines-1).join("\n")
+    }
+
+    if(ISA_DEBUG) console.log("FRONT MATTER ->\n" + JSON.stringify(frontmatter));
+
+    return [frontmatter,data]
+   
+  }
+
+
+    //https://alvinabad.wordpress.com/2008/06/01/dynamically-including-javascript-and-css-files/
+    function includeCSSfile(href) {
+        var head_node = document.getElementsByTagName('head')[0];
+        var link_tag = document.createElement('link');
+        link_tag.setAttribute('rel', 'stylesheet');
+        link_tag.setAttribute('type', 'text/css');
+        link_tag.setAttribute('href', href);
+        head_node.appendChild(link_tag);
+    }   
+
+
+
+    function renderVue(__frontmatter)
+    {
+      __fm = __frontmatter;
+
+      var v_themebox = new Vue({
+        el: '.themebox',
+        data: {
+          themename: __fm["theme"]
+        }
+      })
+
+      var v_themeloader = new Vue({
+        el: '#themeloader',
+        data: {
+          themename: __fm["theme"]
+        }
+      })
+
+      window.v_themeloader = v_themeloader;
+      window.v_themebox = v_themebox;
+
+
+
+    }
+
+
+
+  
+
+  //LOAD Markdown
+  fetch("index.md").then(md=>md.text()).then(md=>{
+      element = document.querySelector(".stackedit__html");
+
+
+      extracted = extract_front_matter(md)
+      
+      //frontmatter stuff
+      fm = extracted[0]
+
+
+      //Dynamically load theme
+      if ("theme" in fm){
+          themename = fm["theme"]
+          //includeCSSfile("/__resources/sampy/themes/"+ themename +".css")
+      }
+      else
+      {
+        fm["theme"] = "default"
+      }
+
+      //render the stuff using VueJS
+      renderVue(fm)
+  
+
+      onlymd = extracted[1]
+
+      element.innerHTML = marked(onlymd)
+
+       // MAKE Table of Content
+          tocbot.init({
+          // Where to render the table of contents.
+          tocSelector: '.stackedit__toc',
+          // Where to grab the headings to build the table of contents.
+          contentSelector: '.stackedit__html',
+          // Which headings to grab inside of the contentSelector element.
+          headingSelector: 'h1, h2, h3',
+          // For headings inside relative or absolute positioned containers within content.
+          hasInnerContainers: true,
+          orderedList: false,
+          });
+  })
+
+
+
+
+  //setInterval(()=>{window.location.reload()},5000)
+
+
--- blog/first/index.md
+++ blog/first/index.md
@@ -1,3 +1,7 @@
+---
+theme: colorful
+---
+
 # Hello I Am Great
 ## World
 
