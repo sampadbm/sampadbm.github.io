bd66a20fa5c0d34492797e218e60f327b45326dc
sampad <smpdmohanty@gmail.com>
2020-07-31 01:23:51 -0700

    Added Katex as default Latex Math renderer

--- __resources/sampy/js/main.js
+++ __resources/sampy/js/main.js
@@ -1,149 +1,131 @@
-//requires marked.min.js, tocbot.min.js, js-yaml.min.js, vue.min.js
+// requires marked.min.js, tocbot.min.js, js-yaml.min.js, vue.min.js
 
+var ISA_DEBUG = 0;
 
-ISA_DEBUG=0
+function extract_front_matter(data) {
+  data = data.trim();
+  var lines = data.split("\n");
 
+  var frontmatter = {};
 
-function extract_front_matter(data){
-    data = data.trim()
-    lines = data.split("\n")
- 
-    frontmatter={};
+  // check if front matter exists
+  if (lines[0].trim().includes("---")) {
+    var nlines = lines.length;
+    lines = lines.splice(1, nlines);
+    var trimmedlines = lines.map((l) => l.trim());
 
-    //check if front matter exists
-    if(lines[0].trim().includes("---"))
-    {
-      nlines = lines.length;
-      lines = lines.splice(1,nlines)
-      trimmedlines = lines.map(l => l.trim())
+    var end = trimmedlines.indexOf("---");
 
-      end = trimmedlines.indexOf("---")
+    var frontmatter_text = lines.splice(0, end).join("");
+    if (ISA_DEBUG) console.log("FRONT MATTER TEXT ->\n" + frontmatter_text);
 
-      frontmatter_text = lines.splice(0,end).join("")
-      if(ISA_DEBUG)console.log("FRONT MATTER TEXT ->\n" + frontmatter_text);
-
-      if(frontmatter_text) {
-          __frontmatter = jsyaml.load(frontmatter_text)
-          if(__frontmatter) frontmatter = __frontmatter;
-      }
-
-      data =   lines.splice(end+1,nlines-1).join("\n")
+    if (frontmatter_text) {
+      var __frontmatter = window.jsyaml.load(frontmatter_text);
+      if (__frontmatter) frontmatter = __frontmatter;
     }
 
-    if(ISA_DEBUG) console.log("FRONT MATTER ->\n" + JSON.stringify(frontmatter));
-
-    return [frontmatter,data]
-   
+    data = lines.splice(end + 1, nlines - 1).join("\n");
   }
 
+  if (ISA_DEBUG) console.log("FRONT MATTER ->\n" + JSON.stringify(frontmatter));
 
-    //https://alvinabad.wordpress.com/2008/06/01/dynamically-including-javascript-and-css-files/
-    function includeCSSfile(href) {
-        var head_node = document.getElementsByTagName('head')[0];
-        var link_tag = document.createElement('link');
-        link_tag.setAttribute('rel', 'stylesheet');
-        link_tag.setAttribute('type', 'text/css');
-        link_tag.setAttribute('href', href);
-        head_node.appendChild(link_tag);
-    }   
+  return [frontmatter, data];
+}
 
+// https://alvinabad.wordpress.com/2008/06/01/dynamically-including-javascript-and-css-files/
+function includeCSSfile(href) {
+  var head_node = document.getElementsByTagName("head")[0];
+  var link_tag = document.createElement("link");
+  link_tag.setAttribute("rel", "stylesheet");
+  link_tag.setAttribute("type", "text/css");
+  link_tag.setAttribute("href", href);
+  head_node.appendChild(link_tag);
+}
 
+function renderVue(__frontmatter) {
+  var __fm = __frontmatter;
 
-    
+  var Vue = window.Vue;
 
-    function renderVue(__frontmatter)
-    {
-      __fm = __frontmatter;
+  var v_themeloader = new Vue({
+    el: "#themeloader",
+    data: {
+      current_theme: __fm.themes[0], // load the first theme
+    },
+  });
 
+  window.v_themeloader = v_themeloader;
 
-      var v_themeloader = new Vue({
-        el: '#themeloader',
-        data: {
-          current_theme: "", //load the first theme
-        },
-       
-      })
+  var v_themebox = new Vue({
+    el: ".themebox",
+    data: {
+      themes_available: __fm.themes,
+    },
+    methods: {
+      loadtheme: function (event) {
+        var selected_theme = event.target.value;
+        if (ISA_DEBUG) console.log("Selected new theme -> " + selected_theme);
+        window.v_themeloader.current_theme = selected_theme;
+      },
+    },
+  });
 
-      window.v_themeloader = v_themeloader
+  window.v_themebox = v_themebox;
+}
 
-      var v_themebox = new Vue({
-        el: '.themebox',
-        data: {
-          themes_available: __fm["themes"],
-        },
-        methods: {
-          loadtheme: function (event) {
-            selected_theme = event.target.value
-            if(ISA_DEBUG) console.log("Selected new theme -> "+ selected_theme)
-            v_themeloader.current_theme = selected_theme
-          }
-        }
-      })
+// LOAD Markdown
+fetch("index.md")
+  .then((md) => md.text())
+  .then((md) => {
+    var element = document.querySelector(".stackedit__html");
 
-      window.v_themebox = v_themebox;
-    
+    var extracted = extract_front_matter(md);
 
+    // frontmatter stuff
+    var fm = extracted[0];
+
+    // Dynamically load theme
+    if ("themes" in fm) {
+      // themename = fm["themes"]
+      // includeCSSfile("/__resources/sampy/themes/"+ themename +".css")
+    } else {
+      //Inject themes = ["default"]
+      fm.themes = ["default"];
     }
 
+    // render the stuff using VueJS
+    renderVue(fm);
 
+    // Actual Markdown Content (no frontmatter)
+    var onlymd = extracted[1];
 
-  
-  //LOAD Markdown
-  fetch("index.md").then(md=>md.text()).then(md=>{
-      element = document.querySelector(".stackedit__html");
+    // render using markedjs
+    // element.innerHTML = marked(onlymd)
 
+    // render using texme + external renderer = markedjs
+    element.innerHTML = window.texme.render(onlymd);
 
-      extracted = extract_front_matter(md)
-      
-      //frontmatter stuff
-      fm = extracted[0]
-
-
-      //Dynamically load theme
-      if ("themes" in fm){
-          //themename = fm["themes"]
-          //includeCSSfile("/__resources/sampy/themes/"+ themename +".css")
-      }
-      else
-      {
-        fm["theme"] = ["default"]
-      }
-
-      //render the stuff using VueJS
-      renderVue(fm)
-  
-      //Actual Markdown Content (no frontmatter)
-      onlymd = extracted[1]
-      
-      //render using markedjs
-      //element.innerHTML = marked(onlymd)
-
-      // render using texme + external renderer = markedjs 
-      element.innerHTML = texme.render(onlymd)
-
-      // MAKE Table of Content
-      tocbot.init({
+    // MAKE Table of Content
+    window.tocbot.init({
       // Where to render the table of contents.
-      tocSelector: '.stackedit__toc',
+      tocSelector: ".stackedit__toc",
       // Where to grab the headings to build the table of contents.
-      contentSelector: '.stackedit__html',
+      contentSelector: ".stackedit__html",
       // Which headings to grab inside of the contentSelector element.
-      headingSelector: 'h1, h2, h3',
+      headingSelector: "h1, h2, h3",
       // For headings inside relative or absolute positioned containers within content.
       hasInnerContainers: true,
       orderedList: false,
-      });
+    });
 
+    // MathJax was never called by texme as we prevented running of texme.renderPage using th4e option -> renderOnLoad: false
+    // Manually run the MathJax Typeset function -> see texme.renderPage to get the idea how it is run
 
-      // MathJax was never called by texme as we prevented running of texme.renderPage using th4e option -> renderOnLoad: false 
-      // Manually run the MathJax Typeset function -> see texme.renderPage to get the idea how it is run
-      outputElement = document.querySelector(".stackedit__html");
-      window.MathJax.Hub.Queue(['Typeset', window.MathJax.Hub, outputElement])
-      
-  })
+    renderMathJax = ()=>{MathJax.Hub.Queue(["Typeset", MathJax.Hub, element])}
+    renderKatex = ()=>{renderMathInElement(document.body)}
+    //renderMathJax()
+    //window.onload = renderMathJax //load when page is ready, recall texme loads MathJax, hence MathJax needs to be loaded when we call renderMathJax
+    window.onload = renderKatex
+  });
 
-
-
-
-  //setInterval(()=>{window.location.reload()},5000)
-
+// setInterval(()=>{window.location.reload()},5000)
--- blog/first/index.md
+++ blog/first/index.md
@@ -2,54 +2,39 @@
 themes: [default,colorful]
 ---
 
-# Hello I Am Great
-## World
+# *Optimal Task Allocation*  
+#### *Autonomous Network Research Group,*
+#### *University of Southern California, Los Angeles*
+---
 
-This is so cool
+### Introduction to the problem
 
-Let me throw some latex math here
+In todays cloud infrastructure, we have thousands of powerful machines (physical or virtual) crunching numbers. To solve a practical task, these machines may need to coordinate and communicate the input data and the intermediate results with many other machines connected over the internet infrastructure before the final results can be obtained. 
 
-$$ e^{i \pi} + 1 = 0. $$
+Each such task might itself consist of subtasks which might have interdependencies and hence a natural ordering of subtasks. The dependencies can be captured in a directed acyclic graph (DAG). Hence a complete task can be represented as a DAG of subtasks. 
 
-```
- This is awesome
+Let us have a task which consists of 4 subtasks T0, T2, T3 and T3.
+Also there are 8 machines M0, M2,...., M7 which are all capable of running the subtasks. These machines have cost of communication between themselves which is captured in a **communication cost matrix** of size 8 x 8.
 
-```
+<p align='center'>
+    <img height="75%" width="75%" src="4_network_costs.png"/>
+    <p align='center'> <u><b>Fig:1 - Network Cost Matrix [in $/GB transfer]</b></u></p>
+</p>
 
-# I am a genius
-## But this is really cool and this is really really long
-## World needs to know this
+<p align='center'>
+    <img height="75%" width="75%" src="dag9.png"/>
+    <p align='center'> <u><b>Fig:2 - Task as DAG</b></u></p>
+</p>
 
+However each of these machines charge a cost for running the subtasks which can be captured in a **compute cost matrix** of size 4 x 8.
 
+<p align='center'>
+    <img height="75%" width="75%" src="4_direct_task_cpu_costs.png"/>
+    <p align='center'> <u><b>Fig:3 - Compute Cost Matrix [in $/subtask]</b></u></p>
+</p>
 
-# Hello I Am Great
-## World
+Also each machine has different processing power and hence takes different time to complete these subtasks. Also time is required to transfer the intermediate results within the subtasks if they are scheduled across different machines. All of this is captured in the **makespan** of the complete task which tells us how long it takes us to execute the whole DAG task.
 
-This is so cool
+*As it happens, we need to keep track of a few more pieces of information ->*
 
-
-```
- This is awesome
-
-```
-
-# I am a genius
-## But this is really cool and this is really really long
-## World needs to know this
-
-
-
-# Hello I Am Great
-## World
-
-This is so cool
-
-
-```
- This is awesome
-
-```
-
-# I am a genius
-## But this is really cool and this is really really long
-## World needs to know this
+$$ ok $$
