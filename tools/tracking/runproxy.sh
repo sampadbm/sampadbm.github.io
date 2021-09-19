b121cfb812c376561802ba45f8bb38db4ce7facc
sampad <sbmohant@usc.edu>
2021-09-18 21:52:08 -0700

    thin+ssl

--- tools/tracking/runproxy.sh
+++ tools/tracking/runproxy.sh
@@ -1,1 +1,3 @@
-rackup -p 9999 --host 0.0.0.0
+#rackup -p 9999 --host 0.0.0.0
+thin start -p 9999 --ssl --ssl-key-file /home/sampad/ee250website/setup/certs/ee250io.tk/privkey.pem --ssl-cert-file /home/sampad/ee250website/setup/certs/ee250io.tk/fullchain.pem
+
