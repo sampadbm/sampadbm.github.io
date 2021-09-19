c291418a4f1840ef4f9072da6c5557fb0dde1017
sampad <sbmohant@usc.edu>
2021-09-18 20:10:00 -0700

    added cors to sinatra in tools/tracking

--- tools/tracking/proxy.rb
+++ tools/tracking/proxy.rb
@@ -4,14 +4,20 @@ require 'httparty'
 require 'sinatra/cors'
 
 
-set :allow_origin, "*"
-set :allow_methods, "GET,HEAD,POST"
-set :allow_headers, "*"
+#set :allow_origin, "*"
+#set :allow_methods, "GET,HEAD,POST"
+#set :allow_headers, "*"
 #set :expose_headers, "*"
 
 headers = { 
   "content-type"  => "application/json",
 }
+
+
+# To enable cross origin requests for all routes:
+configure do
+  enable :cross_origin
+end
 
 post '/forward' do
   payload = request.body.read
