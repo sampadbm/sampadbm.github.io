4e5b160570e74c43c2ba63860c638124d83ab43a
Sampad <sbmohant@usc.edu>
2023-07-29 23:59:10 -0700

    math501 hw9 secant mehtod

--- notes/math501-summer-2023/hw9/secant_method.py
+++ notes/math501-summer-2023/hw9/secant_method.py
@@ -2,11 +2,19 @@ from pylab import *
 from print_color import print
 
 
-def secant_method(f, a, b, iters=100, tolerance=1e-7, debug=False):
+def secant_method(f, a, b, iters=100, tolerance=1e-7, delta=1e-10, debug=False):
     lastb = b + 10 * tolerance
     for it in range(iters):
         fa, fb = f(a), f(b)
         df = (fb - fa) / (b - a)
+
+        # if f is close enough to zero we found our solution
+        if abs(fa) < delta:
+            if debug: print(f"[INFO] abs(f(a)) reached below {delta} at iteration {it} giving us an approximate root! Returning a.", color="green")
+            return a
+        elif abs(fb) < delta:
+            if debug: print(f"[INFO] abs(f(b)) reached below {delta} at iteration {it} giving us an approximate root! Returning b.", color="green")
+            return b
 
         if isclose(df, 0):
             if debug: print(f"[INFO] Slope approached zero, returning current estimate.", color="red")
@@ -26,6 +34,8 @@ def secant_method(f, a, b, iters=100, tolerance=1e-7, debug=False):
 
 
 if __name__ == "__main__":
+
+    print("\n############### Exercise 3.3 | Q5 ###############")
     f = lambda x: x**3 - 5*x + 3
 
     a,b = -3,-1
@@ -41,3 +51,7 @@ if __name__ == "__main__":
     a,b = 1,2
     root = secant_method(f, a, b, debug=True, iters=100000)
     print(f"a={a}, b={b} | root = {root}")
+
+
+    print("\n############### Computer Exercise 3.3 | Q2 ###############")
+    
